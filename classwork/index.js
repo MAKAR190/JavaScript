49684b06b19a653785467be8542fe9f0e94929dd
MK <lutskyimakar@gmail.com>
2019-11-10 14:01:54 +0200

    classwork

---
+++ classwork/index.js
@@ -0,0 +1,87 @@
+'use strict';
+
+// console.log("Hello world");
+
+// let number = 6;
+// let string = "String";
+// let boolean = true; //false
+
+
+// let a=2;
+// let b=3;
+
+// let result =a+b;
+
+// console.log("Our result",result);
+
+// let apple;
+// console.log(apple);
+
+// var a="hello";
+// console.log(typeof a);
+
+// var c=confirm("Дайте денег");
+// console.log(c);
+
+
+// const z=prompt("Enter your name")
+// console.log(z)
+// let a = 9;
+// let b="9 asdf";
+// let result = a % b;
+// console.log(result);
+// // console.log(typef Number(b));
+// let a=Number("adsf");
+// // console.log(Number.parseFloat(a)+50)
+// console.log(Number.isNaN(a));
+// console.log(0.1 + 0.3===0.4);
+
+// // let a=0.1+0.2;
+// // console.log(a.toFixed(1));
+
+// // console.log((Math.random()*10).toFixed(10));
+// const a=1,
+// const b=5,
+// c=8;
+// // console.log(Math.max(a,b,c));
+// console.log(Math.ceil(0.5));
+// console.log(Math.floor(1.5));
+// console.log(Math.round(1.6));
+// console.log(Math.pow(3,2))
+// const age= 25
+// console.log("Hello I`m" + age + "years old");
+// const name="Makar";
+
+// console.log(name.length);
+// console.log(name.indexOf("r"));
+// console.log(name.toLowerCase());
+// console.log(name.toUpperCase());
+// console.log(name.includes("b"));
+// const firstName="Makar";
+// const surName="Lutskii";
+// const age=13;
+// console.log(`Hello my name is ${firstName} ${surName} and im ${age} years old`);
+
+
+// alert(true)
+
+// // console.log(String(true)==="true");
+// console.log(+"100text"); // NaN
+// console.log(+undefined); // NaN
+// console.log(+null); // 0
+// console.log(+true); // 1
+// console.log(+false); // 0
+// console.log( "1" == true); // true
+// console.log( "1" === true); // false
+let name="Генератор защитного поля";
+let price="1000";
+price=2000
+console.log(`Выбран ${name}, цена за штуку ${price} кредитов`);
+
+
+
+
+
+
+
+ 
